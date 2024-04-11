00a765836311e04a79c76eb0836cff65659fd682
lsieversperkins25 <166734330+lsieversperkins25@users.noreply.github.com>
2024-04-11 12:04:02 -0400

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,1 +1,27 @@
+# Example file showing a basic pygame "game loop"
+import pygame
 
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("PaleTurquoise")
+
+    rectangle = pygame.draw.rect(screen, 'green2', [100, 100, 20, 20])
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
