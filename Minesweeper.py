9a5b43f8c2ec962ca408abae22f30d2822ae3a15
lsieversperkins25 <166734330+lsieversperkins25@users.noreply.github.com>
2024-04-12 12:01:28 -0400

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -13,12 +13,39 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos() 
+            
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("PaleTurquoise")
-
-    rectangle = pygame.draw.rect(screen, 'green2', [100, 100, 20, 20])
-
+    more_rectangles = True
+    row = 1
+    column = 1
+    color = 'green3'
+    x1 = 250
+    y1 = 50
+    while more_rectangles == True:
+        if color == 'green3':
+            color = 'green2'
+        else:
+            color = 'green3'
+        if column < 24:
+            pygame.draw.rect(screen, color, [x1, y1, 30, 30])
+            column += 1
+            x1 += 30
+        elif column == 24:
+            pygame.draw.rect(screen, color, [x1, y1, 30, 30])
+            column = 1
+            y1 += 30
+            x1 = 250
+            row += 1
+            if color == 'green3':
+                color = 'green2'
+            else:
+                color = 'green3'
+            if row == 21:
+                more_rectangles = False
+        
     # flip() the display to put your work on screen
     pygame.display.flip()
 
