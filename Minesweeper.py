14efab5cfc9d4f94af8a4056b48fa41f6ecb2995
lsieversperkins25 <166734330+lsieversperkins25@users.noreply.github.com>
2024-04-23 11:54:47 -0400

    Update Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,4 +1,5 @@
 # Example file showing a basic pygame "game loop"
+import random
 import pygame
 
 # pygame setup
@@ -7,6 +8,50 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+screen.fill('PaleTurquoise')
+more_rectangles = True
+row = 1
+column = 1
+color = 'green3'
+x1 = 250
+y1 = 50
+list_of_rectangles = []
+while more_rectangles == True:
+    if color == 'green3':
+        color = 'green2'
+    else:
+        color = 'green3'
+    if column < 24:
+        list_of_rectangles.append( pygame.draw.rect(screen, color, [x1, y1, 30, 30]) )
+        column += 1
+        x1 += 30
+    elif column == 24:
+        list_of_rectangles.append( pygame.draw.rect(screen, color, [x1, y1, 30, 30]) )
+        column = 1
+        y1 += 30
+        x1 = 250
+        row += 1
+        if color == 'green3':
+            color = 'green2'
+        else:
+            color = 'green3'
+        if row == 21:
+            more_rectangles = False
+replicate_list = [thing for thing in list_of_rectangles]
+bombs = 1
+list_bombs = []
+while bombs < 100:
+    explosive = random.randint(0, 479)
+    if explosive not in list_bombs:
+        list_bombs.append(explosive)
+        bombs+=1
+for values in replicate_list:
+    values = 0
+for stuff in list_bombs:
+    replicate_list[stuff] = -1
+
+
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -14,38 +59,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse = pygame.mouse.get_pos() 
+            mouse = pygame.mouse.get_pos()
             
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("PaleTurquoise")
-    more_rectangles = True
-    row = 1
-    column = 1
-    color = 'green3'
-    x1 = 250
-    y1 = 50
-    while more_rectangles == True:
-        if color == 'green3':
-            color = 'green2'
-        else:
-            color = 'green3'
-        if column < 24:
-            pygame.draw.rect(screen, color, [x1, y1, 30, 30])
-            column += 1
-            x1 += 30
-        elif column == 24:
-            pygame.draw.rect(screen, color, [x1, y1, 30, 30])
-            column = 1
-            y1 += 30
-            x1 = 250
-            row += 1
-            if color == 'green3':
-                color = 'green2'
-            else:
-                color = 'green3'
-            if row == 21:
-                more_rectangles = False
-        
+            
+
+    
+                
     # flip() the display to put your work on screen
     pygame.display.flip()
 
